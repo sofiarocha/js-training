2ca64bbcdedf7d2827fe353f181e7529738c6f1b
Sofia <sofiaestanqueirorocha@gmail.com>
2019-04-07 19:48:18 +0100

    continuation of exercises

--- exercises/yell.js
+++ exercises/yell.js
@@ -5,10 +5,14 @@
  * and return the same string in upper case
  *
  */
-
+const yell = (str) => str.toUpperCase();
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof yell, "function");
+assert.strictEqual(yell.length, 1);
+assert.deepStrictEqual(yell("wild code school"), "WILD CODE SCHOOL");
+assert.deepStrictEqual(yell("JavaScript rocks"), "JAVASCRIPT ROCKS");
+
 // End of tests */
